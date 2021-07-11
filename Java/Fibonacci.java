0d7bea3d96774f60be721a3e6e194e1aad403cba
Htou <25567913+Htou@users.noreply.github.com>
2021-07-12 01:45:39 +0200

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -6,14 +6,14 @@ public class Fibonacci {
     if (n <= 1) {
       return n;
     }
-    
+
     ArrayList<Long> list = new ArrayList();
 
     list.add(0L);
     list.add(1L);
 
-    for (int i = 1; i < n; i++) {
-      list.add(list.get(i) + list.get(i - 1));
+    for (int i = 2; i <= n; i++) {
+      list.add(list.get(i - 1) + list.get(i - 2));
     }
 
     System.out.println(list);
@@ -23,7 +23,7 @@ public class Fibonacci {
   public static void main(String args[]) {
 //        Scanner in = new Scanner(System.in);
 //        int n = in.nextInt();
-    int n = 50;
+    int n = 15;
 
     System.out.println(calc_fib(n));
   }
