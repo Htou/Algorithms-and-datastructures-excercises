a5225506ebecf208a376dc0a13a32962befda250
Htou <25567913+Htou@users.noreply.github.com>
2021-07-12 01:35:03 +0200

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -2,29 +2,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Fibonacci {
-    private static long calc_fib(int n) {
-        ArrayList<Long> list = new ArrayList();
+  private static long calc_fib(int n) {
+    ArrayList<Long> list = new ArrayList();
 
-        if (n < 1) {
-            return n;
-        }
-
-        list.add(0L);
-        list.add(1L);
-
-        for (int i = 1; i < n; i++) {
-            list.add(list.get(i - 1) + list.get(i - 2));
-        }
-
-        System.out.println(list);
-        return list.get(n);
+    list.add(0L);
+    list.add(1L);
+      
+    if (n <= 1) {
+      return n;
     }
 
-    public static void main(String args[]) {
+    for (int i = 1; i < n; i++) {
+
+      list.add(list.get(i) + list.get(i - 1));
+    }
+
+    System.out.println(list);
+    return list.get(n);
+  }
+
+  public static void main(String args[]) {
 //        Scanner in = new Scanner(System.in);
 //        int n = in.nextInt();
-        int n = 50;
+    int n = 50;
 
-        System.out.println(calc_fib(n));
-    }
+    System.out.println(calc_fib(n));
+  }
 }
